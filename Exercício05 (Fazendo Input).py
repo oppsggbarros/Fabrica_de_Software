a7a31abf8006dbe3c149dee8e5336a42da9a6ea5
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-05-05 14:36:02 -0400

    Update Exercício05 (Fazendo Input).py

--- Exercício05 (Fazendo Input).py
+++ Exercício05 (Fazendo Input).py
@@ -1,3 +1,7 @@
 name = input(str("Digite seu nome\n"))
 print("Seu nome é:", name)
 print(type(name))
+
+salario = int(input('Salario= '))
+imposto = float(input('Digite o imposto= '))
+x = float(input('digite o número'))
