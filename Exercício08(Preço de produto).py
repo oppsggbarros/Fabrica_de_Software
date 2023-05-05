8c1934f29a061ad89b001e789834d0462d50959e
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-05-05 17:17:47 -0400

    Create Exercício08(Preço de produto).py

---
+++ Exercício08(Preço de produto).py
@@ -0,0 +1,7 @@
+nome = input("Insira o Nome do produto: \n")
+quantidade = int(input("Insira a quantidade do produto: \n"))
+valor = float(input("Insira o valor do produto: \n"))
+desconto = int(input("Insira o desconto: \n"))
+valor_do_produto = valor*quantidade
+total = valor_do_produto*(desconto/100)
+print("Nome do produto:", nome, "\nValor Total do produto:", total)
