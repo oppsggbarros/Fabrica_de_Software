31b4e88d9f154f21a56fdfc39a4ff9e4edf6d7c5
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-05-05 14:55:25 -0400

    Update Exercício05 (Fazendo Input).py

--- Exercício05 (Fazendo Input).py
+++ Exercício05 (Fazendo Input).py
@@ -5,3 +5,6 @@ print(type(name))
 salario = int(input('Salario= '))
 imposto = float(input('Digite o imposto= '))
 x = float(input('digite o número'))
+
+sal = int(input('Salário= '))
+print("O salário digitado foi %d"%sal)
