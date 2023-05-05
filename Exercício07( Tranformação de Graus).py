5124aecb9521834a93f96c5b004aaa65bc576ea6
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-05-05 16:50:00 -0400

    Create Exercício07( Tranformação de Graus).py

---
+++ Exercício07( Tranformação de Graus).py
@@ -0,0 +1,4 @@
+#Transformador de Fahrenheit para Celsius
+f = float(input("Insira a temperatura em Fahrenheit:\n"))
+c = (5/59) * (f-32)
+print("A conversão de", f, "Fahrenheit para Celsius é", c,  "Celsius")
