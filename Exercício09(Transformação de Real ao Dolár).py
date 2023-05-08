b24c00cef89111a8f6a60def214e7ebba38be0c0
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-05-08 14:53:01 -0400

    Create Exercício09(Transformação de Real ao Dolár).py

---
+++ Exercício09(Transformação de Real ao Dolár).py
@@ -0,0 +1,4 @@
+real = float(input("Informe o valor do produto em Reais:\n"))
+dolar = float(input("Informe o valor do Dolár:\n"))
+dinheiro = real*dolar
+print("O valor do produto em dolár foi de %.2f"%dinheiro)
