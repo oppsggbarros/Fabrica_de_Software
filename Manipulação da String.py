fc5fbc43910561cc5f6c60c1157ad03c12cebf48
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-05-08 16:09:41 -0400

    Create Manipulação da String.py

---
+++ Manipulação da String.py
@@ -0,0 +1,10 @@
+a = "Gabriel Gravena"
+b = "123456"
+print(len(a))
+print(a.capitalize()) #Deixa a Primeira letra Maiúscula
+print(a.upper()) #Deixa tudo Maiúsculo
+print(a.casefold()) #Deixa Minúsculo
+print(a.islower()) #Perguntação se a variável é Maiúscula
+print(a.isupper()) #Perguntação se a variável é Minúscula
+print(b.isdigit()) #Vê se tem digito
+print(a.replace("Gravena","Barros")) #Substitui a Variável
