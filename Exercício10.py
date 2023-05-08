17a757a1c60a1bd94347678078d53f481b3d088d
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-05-08 17:21:31 -0400

    Exercícios08/05

---
+++ Exercício10.py
@@ -0,0 +1,10 @@
+texto = str(input("Digite um texto:"))
+print(len(texto))
+
+a = str(input("Digite uma palavra:\n"))
+b = str(input("Digite outra palavra:\n"))
+print("Texto a tem", len(a), "letras")
+print("Texto b tem", len(b), "Letras")
+
+nome = str(input("Digite seu nome:\n"))
+print(nome[:4])
--- Manipulação da String.py
+++ Manipulação da String.py
@@ -1,5 +1,6 @@
 a = "Gabriel Gravena"
 b = "123456"
+s = "Olá, Mundo"
 print(len(a))
 print(a.capitalize()) #Deixa a Primeira letra Maiúscula
 print(a.upper()) #Deixa tudo Maiúsculo
@@ -8,3 +9,10 @@ print(a.islower()) #Perguntação se a variável é Maiúscula
 print(a.isupper()) #Perguntação se a variável é Minúscula
 print(b.isdigit()) #Vê se tem digito
 print(a.replace("Gravena","Barros")) #Substitui a Variável
+print(a.find("Gravena"))
+print(a.rfind("a"))
+print("Gravena"in a)
+print(a.count("a"))
+print(s[-3])
+print(s[1:3])
+print(s[:5])
