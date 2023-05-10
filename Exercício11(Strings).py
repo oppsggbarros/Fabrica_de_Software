398f62dddcb1bdd9b63faf54634399bbde6f72a6
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-05-10 17:28:46 -0400

    Update Exercício11(Strings).py

--- Exercício11(Strings).py
+++ Exercício11(Strings).py
@@ -24,3 +24,22 @@ nota4 = float(input("Digite sua quarta nota: "))
 media = (nota1+nota2+nota3+nota4)/4
 print("Seu nome é %s\nSua série é %s\nSua escola é %s\nSua cidade é %s"% (nome, serie, escola, cidade))
 print("Sua média foi de %f"%media)
+
+raio = float(input("Qual o raio do círculo: "))
+area = raio**2*3.14159
+print(area)
+
+lado = float(input("Digite o lado do quadrado: "))
+areia = lado**2
+print("A area do quadrado é %.1f\n"%areia, "O dobro dessa área é", (areia*2))
+
+salario = float(input("Quanto você ganha por hora trabalhada: "))
+hora = float(input("Quantas horas você trabalhou esse mês: "))
+totales = salario*hora
+print("Você ganhou %.4fR$"%totales)
+
+
+interio1 = int(input("Digite um número interio: "))
+interio2 = int(input("Digite um outro número interio: "))
+real = float(input("Digite um número Real: "))
+print("O valor dos número são respectivamente %d, %d, %.1f"%(interio1, interio2, real))
