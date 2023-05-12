d7ac19fd0506e3ce1068dcc0f81c3f1141b593a3
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-05-12 16:07:31 -0400

    Criação de listas

---
+++ Criação de listas.py
@@ -0,0 +1,27 @@
+lista = [10, 20, 30, 40]
+print(lista)
+print(type(lista))
+
+b = ["Python", 3.5, True, 1, ["sasd", 89.7]]
+print(b[:4])
+print(type(b))
+print(len(b))
+print("Arara" not in b)
+print(89.7 in b)
+lista1 = [1,3,5]
+lista2 = [2,4,6]
+print(lista1+lista2)
+print(max(lista1))#Maior número
+print(min(lista1))#Menor número
+print(sum(lista1))#Soma
+
+uma_lista = ['a', 'b', 'c', 'd', 'e', 'f']
+print(uma_lista[1:3])
+print(uma_lista[:4])
+print(uma_lista[:3])
+print(uma_lista[:])
+print(uma_lista[0:6])
+frutas = ["banana", "maçã", "cereja"]
+frutas[0] = "pera"
+frutas[-1] = "laranjas"
+print(frutas)
