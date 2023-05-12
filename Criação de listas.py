f5625e26c81203468367d40afb213dafde273c75
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-05-12 17:29:25 -0400

    Update Criação de listas.py

--- Criação de listas.py
+++ Criação de listas.py
@@ -23,5 +23,17 @@ print(uma_lista[:])
 print(uma_lista[0:6])
 frutas = ["banana", "maçã", "cereja"]
 frutas[0] = "pera"
-frutas[-1] = "laranjas"
+del frutas[0]
 print(frutas)
+
+list = ["flor", "Azul", [ 1, 'casa']]#é mutável
+list[2][1] = "escola"
+list[2] = [True], 
+list.append(5)#Adiciona no final da lista
+
+print(list)
+
+a = [1,2,3,4,5,6,4]
+a.insert(2,100)
+a.pop(-1)
+print(a)
