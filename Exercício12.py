076b2fff6b5cf3e2fd96cc7f04eebb2c8cc8582c
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-05-12 14:31:49 -0400

    Mais exercícios de string

---
+++ Exercício12.py
@@ -0,0 +1,11 @@
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+c = float(input("Digite o terceiro número: "))
+print((2*a)*(b/2))
+print((3*a)+c)
+print(c**3)
+
+altura = float(input("Informe sua altura: "))
+result = (72.7*altura)-58
+print("Seu peso ideal mínimo é %.2f e o peso ideal máximo é"%result, result+14)
+
