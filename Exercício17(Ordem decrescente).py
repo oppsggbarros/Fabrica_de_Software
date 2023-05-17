bb4c2280615608623381554b5107bdd5535482e3
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-05-17 17:34:49 -0400

    17/05

---
+++ Exercício17(Ordem decrescente).py
@@ -0,0 +1,9 @@
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite um número: "))
+num3 = int(input("Digite um número: "))
+lista = [num1, num2, num3]
+
+
+lista.sort(reverse=True)
+print(lista)
