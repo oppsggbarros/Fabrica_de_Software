bf7b4ed87ad323bca283202d2a85dcf4a73575ed
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-06-05 16:52:44 -0400

    Update Exeções.py

--- Exeções.py
+++ Exeções.py
@@ -1,7 +1,8 @@
 import os
 while True:
     print("Digite o número para ir para a ação")
-    a =input("1- Cadastrar\n2- Cadastro da Passagem\n3-Cadastro do Avioã 5- Imprimir Relatórios\n0- Encerrar\n")
+    a =input("1- Cadastrar\n2- Cadastro do Avião\n3- Cadastro de Passagem\n4- Cadastro da Tripulação\n5- Imprimir Relatórios\n0- Encerrar\n")
+    os.system("cls")
     if a =="1":
         name = input("Informe seu nome: ")
         sobrenome = input("Informe seu sobrenome:")
@@ -21,6 +22,72 @@ while True:
         os.system("pause")
         os.system("cls")
     elif a == "2":
-        print("Nome do passageiro:", name)
-        print("Sobrenome do passageiro:", sobrenome)
-        print("RG do passageiro")
+        modelo = input("Informe o modelo do avião: ")
+        while True:
+            try:
+                ano = int(input("Informe o ano do Avião: "))
+                qntd = int(input("Informe a quantidade de Passageiros: "))
+            except ValueError:
+                print("Coloque somente número!")
+                continue
+            else:
+                break
+        horario = input("Informe as horas de voo: ")
+        cor = input("Qual a cor do Avião: ")
+        os.system("pause")
+        os.system("cls")
+    elif a =="3":
+        destino = input("Informe o Destino da viagem: ")
+        origem = input("Informe a Origem da viagem: ")
+        duração = input("Qual é o tempo total da viagem: ")
+        while True:
+            try:
+                valor = float(input("Qual o valor da passagem: "))
+            except ValueError:
+                print("Infome somente o preço!")
+                continue
+            else:
+                break
+        os.system("pause")
+        os.system("cls")
+    elif a == "4":
+        nometrip = input("Nome da tripulação: ")
+        cargo = input("O cargo da pessoa: ")
+        while True:
+            try:
+                idadetrip = int(input("Idade da tripulação:"))
+                fonetrip = int(input("Telefone da tripulação"))
+            except ValueError:
+                print("Informe somente números: ")
+                continue
+            else:
+                break
+        os.system("pause")
+        os.system("cls")
+    elif a == "5":
+        try:
+            print(name)
+            print(sobrenome)
+            print(rg)
+            print(cpf)
+            print(endereço)
+            print(fone)
+            print(idade)
+            print(destino)
+            print(origem)
+            print(duração)
+            print(valor)
+            print(modelo)
+            print(ano)
+            print(horario)
+            print(cor)
+            print(qntd)
+            print(nometrip)
+            print(idadetrip)
+            print(fonetrip)
+        except NameError:
+            print("Faça o cadastros primeiro!")
+        os.system("pause")
+        os.system("cls")
+    elif a == "0":
+        break
