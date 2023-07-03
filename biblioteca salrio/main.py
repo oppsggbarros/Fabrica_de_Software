ee461c4fd1481859563b89dbc5077fa53111474c
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-07-03 14:10:59 -0400

    03/07

---
+++ biblioteca salrio/main.py
@@ -0,0 +1,4 @@
+import salario
+
+print(salario.salarioAumento(1000,10))
+print(salario.salarioDesconto(1000,10))
---
+++ biblioteca salrio/salario.py
@@ -0,0 +1,6 @@
+def salarioAumento(valor, aumento):
+    novoValor = valor+(valor*aumento/100)
+    return novoValor
+def salarioDesconto(valor, desconto):
+    novoValor = valor-(valor*desconto/100)
+    return novoValor
