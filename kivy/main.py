89f8fd90a393b246890e501afa5e7d439e199d7a
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2023-11-07 17:36:07 -0400

    1

---
+++ kivy/main.py
@@ -0,0 +1,5 @@
+from screenmanagers import *
+
+
+if __name__ == '__main__':
+    MyApp().run()
--- kivy/screenmanagers.py
+++ kivy/screenmanagers.py
@@ -1,119 +1,80 @@
-import kivy
-import random
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import Screen, ScreenManager
+import secrets as sc
 
-class WannaPlayScreen(Screen):
-    def on_yes_pressed(self):
-        self.manager.current = 'guess_choice'
+class Inicio(Screen):
+    def __init__(self, **instance):
+        super().__init__(**instance)
+        layout = BoxLayout(orientation='vertical')
+        label = Label(text="Bem-vindo ao Jogo")
+        button = Button(text="Jogar")
+        button_sair = Button(text='Sair')
+        button_sair.bind(on_press=self.close_app)
+        button.bind(on_press=self.switch_to_info)
+        layout.add_widget(label)
+        layout.add_widget(button)
+        layout.add_widget(button_sair)
+        self.add_widget(layout)
 
-    def on_no_pressed(self):
+    def switch_to_info(self, instance):
+        self.manager.current = 'jogo'
+
+    def close_app(self, instance):
         App.get_running_app().stop()
 
-class GuessesScreen(Screen):
-    def on_yes_pressed(self):
-        self.manager.get_screen('time_to_guess').set_text()
-        self.manager.current = 'time_to_guess'
-        App.get_running_app().guesses_left = 3
-        self.manager.get_screen('time_to_guess').ids.guesses_left.text = App.get_running_app().guess_text
+class In_game(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical')
+        self.label = Label(text="", valign='top')
+        self.text_input = TextInput(hint_text='Digite um número', input_filter='int')
+        button = Button(text="Voltar")
+        button.bind(on_press=self.switch_to_welcome)
+        btn_verifica = Button(text='Verificar')
+        btn_verifica.bind(on_press=self.verifica)
+        btn_reiniciar = Button(text='Reiniciar')
+        btn_reiniciar.bind(on_press=self.reiniciar)
+        layout.add_widget(self.label)
+        layout.add_widget(self.text_input)
+        layout.add_widget(btn_verifica)
+        layout.add_widget(btn_reiniciar)
+        layout.add_widget(button)
+        self.add_widget(layout)
+        self.reiniciar()
 
-class TimeToGuessScreen(Screen):
-    def set_text(self):
-        self.ids.label.text = "Guess a number between 1 and 10"
+    def switch_to_welcome(self, *args):
+        self.manager.current = 'começo'
+        
+    def reiniciar(self, *args):
+        self.number = sc.randbelow(100)
+        self.label.text = ""  
+        self.text_input.text = ""  
+    def verifica(self, instance):
+        guess = int(self.text_input.text)  
+        try:
+            guess = int(self.text_input.text)
+            if guess == self.number:
+                self.label.text = 'Parabéns! Você acertou!'
+            elif guess < self.number:
+                self.label.text = 'O número é maior'
+            elif guess > self.number:
+                self.label.text = 'O número é menor'
+        except ValueError:
+            self.label.text = 'Por favor, insira um número válido.'
+        
+        
+        
 
-    def on_number_pressed(self, number):
-        App.get_running_app().user_guess = number
-        App.get_running_app().guesses_left -= 1
-        self.ids.guesses_left.text = 'Guesses Left: ' + str(App.get_running_app().guesses_left)
-        if App.get_running_app().user_guess == App.get_running_app().random_num:
-            self.manager.current = 'winner'
-        elif (App.get_running_app().user_guess != App.get_running_app().random_num) and (App.get_running_app().guesses_left == 0):
-            self.manager.current = 'loser'
-
-class WinnerScreen(Screen):
-    def on_yes_pressed(self):
-        App.get_running_app().guesses_left = 3
-        self.manager.current = 'guess_choice'
-        App.get_running_app().computer_num = random.randint(1, 10)
-        App.get_running_app().random_num = str(App.get_running_app().computer_num)
-
-    def on_no_pressed(self):
-        App.get_running_app().stop()
-
-class LoserScreen(Screen):
-    def on_yes_pressed(self):
-        self.manager.current = 'guess_choice'
-        App.get_running_app().computer_num = random.randint(1, 10)
-        App.get_running_app().random_num = str(App.get_running_app().computer_num)
-
-    def on_no_pressed(self):
-        App.get_running_app().stop()
-
-class GuessingGameApp(App):
-    computer_num = random.randint(1, 10)
-    random_num = str(computer_num)
-    guesses_left = 3
-    user_guess = None
-    guess_text = ''
-
+class MyApp(App):
     def build(self):
         sm = ScreenManager()
-
-        start_screen = WannaPlayScreen(name='start')
-        start_screen.add_widget(GridLayout(cols=1, children=[
-            Label(text='Want to Play the Guessing Game?'),
-            GridLayout(cols=2, children=[
-                Button(text='Yes', on_release=lambda instance: start_screen.on_yes_pressed()),
-                Button(text='No', on_release=lambda instance: start_screen.on_no_pressed()),
-            ]),
-        ]))
-
-        guess_choice_screen = GuessesScreen(name='guess_choice')
-        guess_choice_screen.add_widget(GridLayout(cols=1, children=[
-            Label(text='You Will Have 3 guesses. Are You Ready?'),
-            Button(text='YES', on_release=lambda instance: guess_choice_screen.on_yes_pressed()),
-        ]))
-
-        time_to_guess_screen = TimeToGuessScreen(name='time_to_guess')
-        time_to_guess_screen.add_widget(GridLayout(cols=1, children=[
-            Label(id='label', text=''),
-            Label(id='guesses_left', text=''),
-            GridLayout(cols=5, children=[
-                Button(text=str(i), on_release=lambda instance, i=i: time_to_guess_screen.on_number_pressed(str(i)))
-                for i in range(1, 11)
-            ]),
-        ]))
-
-        winner_screen = WinnerScreen(name='winner')
-        winner_screen.add_widget(GridLayout(cols=1, children=[
-            Label(text='You have Won!'),
-            Label(text='Would You Like To Play Again?'),
-            GridLayout(cols=2, children=[
-                Button(text='Yes', on_release=lambda instance: winner_screen.on_yes_pressed()),
-                Button(text='No', on_release=lambda instance: winner_screen.on_no_pressed()),
-            ]),
-        ]))
-
-        loser_screen = LoserScreen(name='loser')
-        loser_screen.add_widget(GridLayout(cols=1, children=[
-            Label(text='You have Lost!'),
-            Label(text='Would You Like To Play Again?'),
-            GridLayout(cols=2, children=[
-                Button(text='Yes', on_release=lambda instance: loser_screen.on_yes_pressed()),
-                Button(text='No', on_release=lambda instance: loser_screen.on_no_pressed()),
-            ]),
-        ]))
-
-        sm.add_widget(start_screen)
-        sm.add_widget(guess_choice_screen)
-        sm.add_widget(time_to_guess_screen)
-        sm.add_widget(winner_screen)
-        sm.add_widget(loser_screen)
-
+        sm.add_widget(Inicio(name='começo'))
+        sm.add_widget(In_game(name='jogo'))
         return sm
 
 if __name__ == '__main__':
-    GuessingGameApp().run()
+    MyApp().run()
