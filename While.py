64ea6f1274e5f5fe9a91e274cc4d2c8d4775a7bb
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2024-06-24 09:18:00 -0400

    a

--- While.py
+++
@@ -1,9 +0,0 @@
-'''while True:
-    print("Loop Infinito\n")''' 
-while True:
-    valor=int(input("Digite o valor 1 ou 0 para encerrar: "))
-    if valor <=1:
-        continue
-        print("Maior que um")
-    if valor >=1:
-        print("Maior que um")
